5336a658a6541fbe961d31cf6ab03b8a62370a2b
alditl <aldanabelen.torres30@gmail.com>
2021-09-29 19:08:28 -0300

    Login y register

--- app.js
+++ app.js
@@ -17,4 +17,12 @@ app.get("/empty-cart",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/empty-cart.html"))
 });
 
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+});
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+});
+
 app.listen(3000,()=>console.log("server corriendo en el puerto 3000"));
