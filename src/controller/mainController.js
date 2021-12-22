c1a625ab0cfe30db1e8d0a359eff0d4f35dd56cb
Pablo Seoane <pablitoseoane@gmail.com>
2021-12-22 11:13:53 -0300

    Solucuina vista Home cat destacados

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -2,25 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const db = require ('../database/models')
 
-/* Logica para traer los productos */
-let jsonProducts = fs.readFileSync(path.resolve(__dirname, '../data/products.json'), 'utf-8');
-let products = JSON.parse(jsonProducts); //Convertimos el json a array
 
 let controller = {
     home: (req,res) => {
-        let destacados=[];
-       products.forEach(product =>{
-           if (product.category.indexOf('destacados')!=-1) {
-            if (destacados.length <=4) {
-                destacados.push(product);
-            }   
-            
-           }
-       });
-       
-       res.render('home',{destacados});
-
+       db.Category.findOne( 
+        {where: {id:2}, 
+        include: [{association:"products"}]}) 
+    .then(function(destacados){
+        res.render('home',{destacados:destacados.products})
+    })
     },
+
     about: (req,res) => {
         res.render('about');
     }
